910a1fcd3bce14a33955d9cdd49d0fa64e311c8c
tanitaka_tech <78785830+tanitaka-tech@users.noreply.github.com>
2024-05-08 12:31:40 +0900

    ♻️ DependencyVariable内の細かい命名を修正

--- Runtime/Variable/DependencyVariable/DependencyVariable.cs
+++ Runtime/Variable/DependencyVariable/DependencyVariable.cs
@@ -6,21 +6,21 @@ namespace TanitakaTech.StateVariable
     public class DependencyVariable<T> : IVariableObserver<T>
     {
         public T Read() => ResultSelector();
-        public Observable<T> Observe() => CombineLatestObservable;
+        public Observable<T> Observe() => ResultObservable;
         
-        private Observable<T> CombineLatestObservable { get; }
+        private Observable<T> ResultObservable { get; }
         private Func<T> ResultSelector { get; }
         
-        public DependencyVariable(Observable<T> combineLatestObservable, Func<T> resultSelector)
+        public DependencyVariable(Observable<T> resultObservable, Func<T> resultSelector)
         {
             ResultSelector = resultSelector;
-            CombineLatestObservable = combineLatestObservable;
+            ResultObservable = resultObservable;
         }
         
         public static DependencyVariable<TR> Create<T1, TR>(IVariableObserver<T1> source1, Func<T1, TR> resultSelector)
         {
             return new DependencyVariable<TR>(
-                combineLatestObservable: source1.Observe().Select(resultSelector),
+                resultObservable: source1.Observe().Select(resultSelector),
                 resultSelector: () => resultSelector(source1.Read())
             );
         }
@@ -28,7 +28,7 @@ namespace TanitakaTech.StateVariable
         public static DependencyVariable<TR> Create<T1, T2, TR>(IVariableObserver<T1> source1, IVariableObserver<T2> source2, Func<T1, T2, TR> resultSelector)
         {
             return new DependencyVariable<TR>(
-                combineLatestObservable: ObservableExtensions.CombineLatest(
+                resultObservable: ObservableExtensions.CombineLatest(
                     source1.Observe(), source2.Observe(),
                     resultSelector
                 ),
@@ -39,7 +39,7 @@ namespace TanitakaTech.StateVariable
         public static DependencyVariable<TR> Create<T1, T2, T3, TR>(IVariableObserver<T1> source1, IVariableObserver<T2> source2, IVariableObserver<T3> source3, Func<T1, T2, T3, TR> resultSelector)
         {
             return new DependencyVariable<TR>(
-                combineLatestObservable: ObservableExtensions.CombineLatest(
+                resultObservable: ObservableExtensions.CombineLatest(
                     source1.Observe(), source2.Observe(), source3.Observe(), 
                     resultSelector
                     ),
@@ -50,7 +50,7 @@ namespace TanitakaTech.StateVariable
         public static DependencyVariable<TR> Create<T1, T2, T3, T4, TR>(IVariableObserver<T1> source1, IVariableObserver<T2> source2, IVariableObserver<T3> source3, IVariableObserver<T4> source4, Func<T1, T2, T3, T4, TR> resultSelector)
         {
             return new DependencyVariable<TR>(
-                combineLatestObservable: ObservableExtensions.CombineLatest(
+                resultObservable: ObservableExtensions.CombineLatest(
                     source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(),
                     resultSelector
                 ),
@@ -61,7 +61,7 @@ namespace TanitakaTech.StateVariable
         public static DependencyVariable<TR> Create<T1, T2, T3, T4, T5, TR>(IVariableObserver<T1> source1, IVariableObserver<T2> source2, IVariableObserver<T3> source3, IVariableObserver<T4> source4, IVariableObserver<T5> source5, Func<T1, T2, T3, T4, T5, TR> resultSelector)
         {
             return new DependencyVariable<TR>(
-                combineLatestObservable: ObservableExtensions.CombineLatest(
+                resultObservable: ObservableExtensions.CombineLatest(
                     source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(), source5.Observe(),
                     resultSelector
                 ),
@@ -72,7 +72,7 @@ namespace TanitakaTech.StateVariable
         public static DependencyVariable<TR> Create<T1, T2, T3, T4, T5, T6, TR>(IVariableObserver<T1> source1, IVariableObserver<T2> source2, IVariableObserver<T3> source3, IVariableObserver<T4> source4, IVariableObserver<T5> source5, IVariableObserver<T6> source6, Func<T1, T2, T3, T4, T5, T6, TR> resultSelector)
         {
             return new DependencyVariable<TR>(
-                combineLatestObservable: ObservableExtensions.CombineLatest(
+                resultObservable: ObservableExtensions.CombineLatest(
                     source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(), source5.Observe(), source6.Observe(),
                     resultSelector
                 ),
@@ -83,7 +83,7 @@ namespace TanitakaTech.StateVariable
         public static DependencyVariable<TR> Create<T1, T2, T3, T4, T5, T6, T7, TR>(IVariableObserver<T1> source1, IVariableObserver<T2> source2, IVariableObserver<T3> source3, IVariableObserver<T4> source4, IVariableObserver<T5> source5, IVariableObserver<T6> source6, IVariableObserver<T7> source7, Func<T1, T2, T3, T4, T5, T6, T7, TR> resultSelector)
         {
             return new DependencyVariable<TR>(
-                combineLatestObservable: ObservableExtensions.CombineLatest(
+                resultObservable: ObservableExtensions.CombineLatest(
                     source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(), source5.Observe(), source6.Observe(), source7.Observe(),
                     resultSelector
                 ),
@@ -94,7 +94,7 @@ namespace TanitakaTech.StateVariable
         public static DependencyVariable<TR> Create<T1, T2, T3, T4, T5, T6, T7, T8, TR>(IVariableObserver<T1> source1, IVariableObserver<T2> source2, IVariableObserver<T3> source3, IVariableObserver<T4> source4, IVariableObserver<T5> source5, IVariableObserver<T6> source6, IVariableObserver<T7> source7, IVariableObserver<T8> source8, Func<T1, T2, T3, T4, T5, T6, T7, T8, TR> resultSelector)
         {
             return new DependencyVariable<TR>(
-                combineLatestObservable: ObservableExtensions.CombineLatest(
+                resultObservable: ObservableExtensions.CombineLatest(
                     source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(), source5.Observe(), source6.Observe(), source7.Observe(), source8.Observe(),
                     resultSelector
                 ),
@@ -105,7 +105,7 @@ namespace TanitakaTech.StateVariable
         public static DependencyVariable<TR> Create<T1, T2, T3, T4, T5, T6, T7, T8, T9, TR>(IVariableObserver<T1> source1, IVariableObserver<T2> source2, IVariableObserver<T3> source3, IVariableObserver<T4> source4, IVariableObserver<T5> source5, IVariableObserver<T6> source6, IVariableObserver<T7> source7, IVariableObserver<T8> source8, IVariableObserver<T9> source9, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TR> resultSelector)
         {
             return new DependencyVariable<TR>(
-                combineLatestObservable: ObservableExtensions.CombineLatest(
+                resultObservable: ObservableExtensions.CombineLatest(
                     source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(), source5.Observe(), source6.Observe(), source7.Observe(), source8.Observe(), source9.Observe(),
                     resultSelector
                 ),
@@ -120,7 +120,7 @@ namespace TanitakaTech.StateVariable
             IVariableObserver<T10> source10, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TR> resultSelector)
         {
             return new DependencyVariable<TR>(
-                combineLatestObservable: ObservableExtensions.CombineLatest(
+                resultObservable: ObservableExtensions.CombineLatest(
                     source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(), source5.Observe(),
                     source6.Observe(), source7.Observe(), source8.Observe(), source9.Observe(), source10.Observe(),
                     resultSelector
