0d83ea93298ffbd001942de1fa3d4a1a6f70a35e
tanitaka_tech <78785830+tanitaka-tech@users.noreply.github.com>
2024-07-09 06:06:17 +0900

    🔥 Request周りの処理を削除

--- Runtime/Request/IRequestPusher.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TanitakaTech.StateVariable
-{
-    public interface IRequestPusher<TRequest> where TRequest : class
-    {
-        public void PushRequest(TRequest requestValue);
-    }
-}
