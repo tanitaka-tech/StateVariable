c9c865ffb28fa16f6dd6e4742350f9975ded42c6
tanitaka_tech <78785830+tanitaka-tech@users.noreply.github.com>
2024-09-03 21:06:42 +0900

    🔧 Unityへの依存を切り離し

--- Runtime/VariableCollection/IVariableCollectionElementSetter.cs
+++ Runtime/VariableCollection/IVariableCollectionElementSetter.cs
@@ -1,8 +1,10 @@
-﻿namespace TanitakaTech.StateVariable.VariableCollection
+﻿using System;
+
+namespace TanitakaTech.StateVariable.VariableCollection
 {
-    public interface IVariableCollectionElementSetter<TKey, TValue> :
-        IVariableCollectionElementReader<TKey, TValue>
+    public interface IVariableCollectionElementSetter<TKey, TValue> : IVariableCollectionElementReader<TKey, TValue>
     {
         void SetElement(TKey id, TValue newElement);
+        void SetElementWithPrevious(TKey id, Func<(bool hasPreviousValue, TValue previousElement), TValue> newElementSelector);
     }
 }
