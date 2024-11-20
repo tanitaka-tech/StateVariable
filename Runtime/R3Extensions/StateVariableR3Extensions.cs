bb8b78a01376984aa612a77877eb7e10ef36249e
tanitaka_tech <78785830+tanitaka-tech@users.noreply.github.com>
2024-11-20 22:11:59 +0900

    refactor: 細かい命名を修正

--- Runtime/R3Extensions/StateVariableR3Extensions.cs
+++ Runtime/R3Extensions/StateVariableR3Extensions.cs
@@ -5,7 +5,7 @@ namespace TanitakaTech.StateVariable.R3Extensions
 {
     public static class StateVariableR3Extensions
     {
-        public static IStateObserver<T> ToVariableObserver<T>(this Observable<T> observable, out IDisposable disposable)
+        public static IStateObserver<T> ToStateObserver<T>(this Observable<T> observable, out IDisposable disposable)
         {
             var bridge = new StateObserverBridge<T>(observable, out disposable);
             return bridge;
--- Runtime/State.cs
+++ Runtime/State.cs
@@ -3,14 +3,14 @@
     public class State<T> :
         IStateSetter<T>
     {
-        private T _variable;
+        private T _value;
 
         public State(T initialValue)
         {
-            _variable = initialValue;
+            _value = initialValue;
         }
         
-        public void Set(T value) => _variable = value;
-        public T Read() => _variable;
+        public void Set(T value) => _value = value;
+        public T Read() => _value;
     }
 }
