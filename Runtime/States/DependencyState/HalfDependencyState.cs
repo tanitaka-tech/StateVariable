9561f10e5d8066dcab3a71cea6fe3fe267c5726b
tanitaka_tech <78785830+tanitaka-tech@users.noreply.github.com>
2025-09-30 23:35:00 +0900

    feat: HalfDependencyStateでObservableを受け入れられるようにした

--- Runtime/States/DependencyState/HalfDependencyState.cs
+++ Runtime/States/DependencyState/HalfDependencyState.cs
@@ -28,102 +28,153 @@ namespace TanitakaTech.StateVariable
 
         public static HalfDependencyState<Tr> Create<Tr>(Tr initialValue, Func<Tr, Tr> resultSelector)
         {
-            var selfObservableVariable = new ObservableState<Tr>(initialValue);
+            var selfObservableState = new ObservableState<Tr>(initialValue);
             return new HalfDependencyState<Tr>(
-                selfObservableState: selfObservableVariable,
-                resultObservable: selfObservableVariable.Observe().Select(resultSelector).DistinctUntilChanged(),
-                resultSelector: () => resultSelector(selfObservableVariable.Read())
+                selfObservableState: selfObservableState,
+                resultObservable: selfObservableState.Observe().Select(resultSelector).DistinctUntilChanged(),
+                resultSelector: () => resultSelector(selfObservableState.Read())
+            );
+        }
+
+        public static HalfDependencyState<Tr> Create<T1, Tr>(Tr initialValue, Observable<T1> source1, Func<Tr, T1, Tr> resultSelector)
+        {
+            var selfObservableState = new ObservableState<Tr>(initialValue);
+            T1 source1BeforeValue = default;
+            return new HalfDependencyState<Tr>(
+                selfObservableState: selfObservableState,
+                resultObservable: Observable.CombineLatest(
+                    selfObservableState.Observe(),
+                    source1.Do(v => source1BeforeValue = v),
+                    resultSelector
+                ).DistinctUntilChanged(),
+                resultSelector: () => resultSelector(selfObservableState.Read(), source1BeforeValue)
+            );
+        }
+
+        public static HalfDependencyState<Tr> Create<T1, T2, Tr>(Tr initialValue, Observable<T1> source1, Observable<T2> source2, Func<Tr, T1, T2, Tr> resultSelector)
+        {
+            var selfObservableState = new ObservableState<Tr>(initialValue);
+            T1 source1BeforeValue = default;
+            T2 source2BeforeValue = default;
+            return new HalfDependencyState<Tr>(
+                selfObservableState: selfObservableState,
+                resultObservable: Observable.CombineLatest(
+                    selfObservableState.Observe(),
+                    source1.Do(v => source1BeforeValue = v),
+                    source2.Do(v => source2BeforeValue = v),
+                    resultSelector
+                ).DistinctUntilChanged(),
+                resultSelector: () => resultSelector(selfObservableState.Read(), source1BeforeValue, source2BeforeValue)
+            );
+        }
+
+        public static HalfDependencyState<Tr> Create<T1, T2, T3, Tr>(Tr initialValue, Observable<T1> source1, Observable<T2> source2, Observable<T3> source3, Func<Tr, T1, T2, T3, Tr> resultSelector)
+        {
+            var selfObservableState = new ObservableState<Tr>(initialValue);
+            T1 source1BeforeValue = default;
+            T2 source2BeforeValue = default;
+            T3 source3BeforeValue = default;
+            return new HalfDependencyState<Tr>(
+                selfObservableState: selfObservableState,
+                resultObservable: Observable.CombineLatest(
+                    selfObservableState.Observe(),
+                    source1.Do(v => source1BeforeValue = v),
+                    source2.Do(v => source2BeforeValue = v),
+                    source3.Do(v => source3BeforeValue = v),
+                    resultSelector
+                ).DistinctUntilChanged(),
+                resultSelector: () => resultSelector(selfObservableState.Read(), source1BeforeValue, source2BeforeValue, source3BeforeValue)
             );
         }
 
         public static HalfDependencyState<Tr> Create<T1, Tr>(Tr initialValue, IStateObserver<T1> source1, Func<Tr, T1, Tr> resultSelector)
         {
-            var selfObservableVariable = new ObservableState<Tr>(initialValue);
+            var selfObservableState = new ObservableState<Tr>(initialValue);
             return new HalfDependencyState<Tr>(
-                selfObservableState: selfObservableVariable,
+                selfObservableState: selfObservableState,
                 resultObservable: Observable.CombineLatest(
-                    selfObservableVariable.Observe(), source1.Observe(),
+                    selfObservableState.Observe(), source1.Observe(),
                     resultSelector
                 ).DistinctUntilChanged(),
-                resultSelector: () => resultSelector(selfObservableVariable.Read(), source1.Read())
+                resultSelector: () => resultSelector(selfObservableState.Read(), source1.Read())
             );
         }
 
         public static HalfDependencyState<Tr> Create<T1, T2, Tr>(Tr initialValue, IStateObserver<T1> source1, IStateObserver<T2> source2, Func<Tr, T1, T2, Tr> resultSelector)
         {
-            var selfObservableVariable = new ObservableState<Tr>(initialValue);
+            var selfObservableState = new ObservableState<Tr>(initialValue);
             return new HalfDependencyState<Tr>(
-                selfObservableState: selfObservableVariable,
+                selfObservableState: selfObservableState,
                 resultObservable: Observable.CombineLatest(
-                    selfObservableVariable.Observe(), source1.Observe(), source2.Observe(),
+                    selfObservableState.Observe(), source1.Observe(), source2.Observe(),
                     resultSelector
                 ).DistinctUntilChanged(),
-                resultSelector: () => resultSelector(selfObservableVariable.Read(), source1.Read(), source2.Read())
+                resultSelector: () => resultSelector(selfObservableState.Read(), source1.Read(), source2.Read())
             );
         }
 
         public static HalfDependencyState<Tr> Create<T1, T2, T3, Tr>(Tr initialValue, IStateObserver<T1> source1, IStateObserver<T2> source2, IStateObserver<T3> source3, Func<Tr, T1, T2, T3, Tr> resultSelector)
         {
-            var selfObservableVariable = new ObservableState<Tr>(initialValue);
+            var selfObservableState = new ObservableState<Tr>(initialValue);
             return new HalfDependencyState<Tr>(
-                selfObservableState: selfObservableVariable,
+                selfObservableState: selfObservableState,
                 resultObservable: Observable.CombineLatest(
-                    selfObservableVariable.Observe(), source1.Observe(), source2.Observe(), source3.Observe(),
+                    selfObservableState.Observe(), source1.Observe(), source2.Observe(), source3.Observe(),
                     resultSelector
                 ).DistinctUntilChanged(),
-                resultSelector: () => resultSelector(selfObservableVariable.Read(), source1.Read(), source2.Read(), source3.Read())
+                resultSelector: () => resultSelector(selfObservableState.Read(), source1.Read(), source2.Read(), source3.Read())
             );
         }
 
         public static HalfDependencyState<Tr> Create<T1, T2, T3, T4, Tr>(Tr initialValue, IStateObserver<T1> source1, IStateObserver<T2> source2, IStateObserver<T3> source3, IStateObserver<T4> source4, Func<Tr, T1, T2, T3, T4, Tr> resultSelector)
         {
-            var selfObservableVariable = new ObservableState<Tr>(initialValue);
+            var selfObservableState = new ObservableState<Tr>(initialValue);
             return new HalfDependencyState<Tr>(
-                selfObservableState: selfObservableVariable,
+                selfObservableState: selfObservableState,
                 resultObservable: Observable.CombineLatest(
-                    selfObservableVariable.Observe(), source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(),
+                    selfObservableState.Observe(), source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(),
                     resultSelector
                 ).DistinctUntilChanged(),
-                resultSelector: () => resultSelector(selfObservableVariable.Read(), source1.Read(), source2.Read(), source3.Read(), source4.Read())
+                resultSelector: () => resultSelector(selfObservableState.Read(), source1.Read(), source2.Read(), source3.Read(), source4.Read())
             );
         }
 
         public static HalfDependencyState<Tr> Create<T1, T2, T3, T4, T5, Tr>(Tr initialValue, IStateObserver<T1> source1, IStateObserver<T2> source2, IStateObserver<T3> source3, IStateObserver<T4> source4, IStateObserver<T5> source5, Func<Tr, T1, T2, T3, T4, T5, Tr> resultSelector)
         {
-            var selfObservableVariable = new ObservableState<Tr>(initialValue);
+            var selfObservableState = new ObservableState<Tr>(initialValue);
             return new HalfDependencyState<Tr>(
-                selfObservableState: selfObservableVariable,
+                selfObservableState: selfObservableState,
                 resultObservable: Observable.CombineLatest(
-                    selfObservableVariable.Observe(), source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(), source5.Observe(),
+                    selfObservableState.Observe(), source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(), source5.Observe(),
                     resultSelector
                 ).DistinctUntilChanged(),
-                resultSelector: () => resultSelector(selfObservableVariable.Read(), source1.Read(), source2.Read(), source3.Read(), source4.Read(), source5.Read())
+                resultSelector: () => resultSelector(selfObservableState.Read(), source1.Read(), source2.Read(), source3.Read(), source4.Read(), source5.Read())
             );
         }
 
         public static HalfDependencyState<Tr> Create<T1, T2, T3, T4, T5, T6, Tr>(Tr initialValue, IStateObserver<T1> source1, IStateObserver<T2> source2, IStateObserver<T3> source3, IStateObserver<T4> source4, IStateObserver<T5> source5, IStateObserver<T6> source6, Func<Tr, T1, T2, T3, T4, T5, T6, Tr> resultSelector)
         {
-            var selfObservableVariable = new ObservableState<Tr>(initialValue);
+            var selfObservableState = new ObservableState<Tr>(initialValue);
             return new HalfDependencyState<Tr>(
-                selfObservableState: selfObservableVariable,
+                selfObservableState: selfObservableState,
                 resultObservable: Observable.CombineLatest(
-                    selfObservableVariable.Observe(), source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(), source5.Observe(), source6.Observe(),
+                    selfObservableState.Observe(), source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(), source5.Observe(), source6.Observe(),
                     resultSelector
                 ).DistinctUntilChanged(),
-                resultSelector: () => resultSelector(selfObservableVariable.Read(), source1.Read(), source2.Read(), source3.Read(), source4.Read(), source5.Read(), source6.Read())
+                resultSelector: () => resultSelector(selfObservableState.Read(), source1.Read(), source2.Read(), source3.Read(), source4.Read(), source5.Read(), source6.Read())
             );
         }
 
         public static HalfDependencyState<Tr> Create<T1, T2, T3, T4, T5, T6, T7, Tr>(Tr initialValue, IStateObserver<T1> source1, IStateObserver<T2> source2, IStateObserver<T3> source3, IStateObserver<T4> source4, IStateObserver<T5> source5, IStateObserver<T6> source6, IStateObserver<T7> source7, Func<Tr, T1, T2, T3, T4, T5, T6, T7, Tr> resultSelector)
         {
-            var selfObservableVariable = new ObservableState<Tr>(initialValue);
+            var selfObservableState = new ObservableState<Tr>(initialValue);
             return new HalfDependencyState<Tr>(
-                selfObservableState: selfObservableVariable,
+                selfObservableState: selfObservableState,
                 resultObservable: Observable.CombineLatest(
-                    selfObservableVariable.Observe(), source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(), source5.Observe(), source6.Observe(), source7.Observe(),
+                    selfObservableState.Observe(), source1.Observe(), source2.Observe(), source3.Observe(), source4.Observe(), source5.Observe(), source6.Observe(), source7.Observe(),
                     resultSelector
                 ).DistinctUntilChanged(),
-                resultSelector: () => resultSelector(selfObservableVariable.Read(), source1.Read(), source2.Read(), source3.Read(), source4.Read(), source5.Read(), source6.Read(), source7.Read())
+                resultSelector: () => resultSelector(selfObservableState.Read(), source1.Read(), source2.Read(), source3.Read(), source4.Read(), source5.Read(), source6.Read(), source7.Read())
             );
         }
     }
